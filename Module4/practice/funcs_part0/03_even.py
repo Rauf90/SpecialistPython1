8841101b74d50943c1630905e3593b7d8e609a46
Rauf_Sayfutdinov <106909928+RaufSayfutdinov@users.noreply.github.com>
2022-06-19 15:21:46 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -1,9 +1,10 @@
-def even(...):
-    ...
-    return ...
+def even(n):
+    if n % 2 == 0:
+        return n
 
-n = ...
+
+n = int(input("n:"))
 if even(n):
-   print("Число четное")
+    print("Число четное")
 else:
-   print("Число не четное")
+    print("Число не четное")
